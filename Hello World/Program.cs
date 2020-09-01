d99b722c88c777b7fd21805245649d2b1d14cdf9
Florian Lutterbach <lutterbachf@gmail.com>
2020-09-01 09:58:04 +0200

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,28 @@
+﻿using Hello_World;
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var book = new Book("New Book");
+      book.AddGrade(12.6);
+      book.AddGrade(10.3);
+      book.AddGrade(6.11);
+      book.AddGrade(4.1);
+      book.AddGrade(56.1);
+
+      var stats = book.GetStatistics();
+      Console.WriteLine($"The lowest grade is{stats.Low}");
+      Console.WriteLine($"The highest grade is{stats.High}");
+      Console.WriteLine($"The average grade is{stats.Average}");
+      Console.ReadLine();
+
+
+    }
+  }
+}
